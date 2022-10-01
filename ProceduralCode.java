bbe299f03558fbbcebff274514809b416ea98bb6
tayyaba4u <113998279+tayyaba4u@users.noreply.github.com>
2022-10-02 01:50:47 +0500

    Add files via upload

---
+++ ProceduralCode.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ProceduralCode
+{
+public static void main (String args [])
+{
+double fixedbalance;
+Scanner input = new Scanner (System.in);  
+System.out.println("Enter the amount: ");  
+fixedbalance = input.nextDouble();
+
+double totalEvents;
+System.out.println("Enter the total events you will be organizing: ");
+totalEvents = input.nextDouble();
+double dividedbudget = fixedbalance/totalEvents;
+System.out.println(dividedbudget);
+//put if else or switch statment according to the budget and display the options.
+//if (dividedbudget=100,000)
+//{
+//System.out.println("");
+//{
+
+    
+
+}
+}
